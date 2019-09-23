3e58885bc8fad3f64e042dda4dc259befb5f7150
Joellehelm <joellenhelm@gmail.com>
2019-09-23 16:57:57 -0500

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,25 @@
 class Animal
+attr_accessor :weight
+attr_reader :species, :nickname, :zoo
+
+@@all = []
+    def initialize(species, weight, nickname, zoo)
+      @species = species
+      @weight = weight
+      @nickname = nickname
+      @zoo = zoo
+      @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+
+    def self.find_by_species(sp)
+        Animal.all.map { |animal| animal.species == sp}
+    end
+    
+
 
 end
--- run.rb
+++ run.rb
@@ -6,5 +6,27 @@ require 'pry'
 #Test your code here
 
 
-binding.pry
-puts "done"
+# binding.pry
+# puts "done"
+
+
+austin = Zoo.new("austin zoo", "austin tx")
+sf = Zoo.new("sf zoo", "san fransisco")
+nola = Zoo.new("new orleans zoo", "new orleans")
+nolatwo = Zoo.new("nola 2", "new orleans")
+
+zebra = Animal.new("mammal", 500, "zebra", austin)
+otter = Animal.new("mammal", 45, "otter", austin)
+snake = Animal.new("reptile", 13, "snake", austin)
+rabbit = Animal.new("mammal", 5, "rabbit", nola)
+
+
+# puts austin.location
+# puts Animal.find_by_species("mammal")
+# puts zebra.zoo
+# puts austin.animals
+# puts austin.animal_species
+# puts austin.find_by_species("mammal")
+# puts austin.animal_nicknames
+
+# p Zoo.find_by_location("new orleans")
